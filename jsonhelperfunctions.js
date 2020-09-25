48f5a57af0f7dc5db682ead3dd7ffd3cbd7c73ab
Adharsh sabu <44582238+raven-97@users.noreply.github.com>
2020-09-25 23:18:22 +0530

    Update jsonhelperfunctions.js

--- jsonhelperfunctions.js
+++ jsonhelperfunctions.js
@@ -1,18 +1,19 @@
 //global variables
-
-
+var allDate=[];
+var date,statecode;
 
 function getData(date,statecode){
     //converting string to date format to make subracting date easier
     var ndate=new Date(date);
     //fetch api to fetch json object from url
-    return fetch('https://api.covid19india.org/v4/timeseries.json')
+    fetch('https://api.covid19india.org/v4/timeseries.json')
         .then(res => res.json())
         .then((out) => {
             var data=[],isodate,nconfirmed,nrecovered,ndeceased;
             for(let i=7;i>0;i--){
                 //subtracting date
                 ndate.setDate(ndate.getDate() - i);
+              
                 //converting date back to iso format
                 year = ndate.getFullYear();
                 month = ndate.getMonth()+1;
@@ -25,6 +26,7 @@ function getData(date,statecode){
                     month = '0' + month;
                 }
                 isodate=year+'-' + month + '-'+dt;
+              allDate.push(isodate);
                 //if data is not present
                 if(out[statecode].dates[isodate].delta==undefined){
                     nconfirmed=nrecovered=ndeceased=0;
@@ -49,7 +51,71 @@ function getData(date,statecode){
                 //reseting date back to original date
                 ndate=new Date(date);
             }   
-            return data;
-            //console.log( data);
-    }).catch(err => console.error(err));
+            console.log(data);
+      var dataPoints1 = [],dataPoints2 = [],dataPoints3 = [];
+      var options =  {
+        animationEnabled: true,
+        theme: "light2",
+        title: {
+          text: "Covid Data"
+        },
+        axisX: {
+			title: "Days",
+          valueFormatString: "DD MMM YY",
+		  titleFontSize: 24
+        },
+        axisY: {
+          title: "Count",
+          titleFontSize: 24
+        },
+        data: [{
+          type: "spline", 
+          dataPoints: dataPoints1
+        },{
+          type: "spline", 
+          dataPoints: dataPoints2
+        },{
+          type: "spline", 
+          dataPoints: dataPoints3
+        }]
+      };
+
+      for (var i = 0; i < data.length; i++) {
+		dataPoints1.push({
+			x: new Date(allDate[i]),
+			y: data[i]['confirmed']
+		});
+		dataPoints2.push({
+			x: new Date(allDate[i]),
+			y: data[i]['deceased']
+		});
+		dataPoints3.push({
+			x: new Date(allDate[i]),
+			y: data[i]['recovered']
+		});
+        
+	}
+      try
+        {
+          $('#chartContainer').CanvasJSChart(options);
+        }
+      catch(e)
+        {
+          console.log(e.name,e.message );
+        }
+        
+    
+          }).catch(err => console.error(err));
 }
+$(document).ready( ()=>{
+  console.log("ready");
+  $("#today, #state").on('change',()=> {
+    var $date = $('#today').val();
+    var $state = $('#state').val();
+    //call getData only if both state and date have been changed
+    if($date!="" && $state!="DF")
+      {
+        getData($date,$state);
+      }  
+});
+});
